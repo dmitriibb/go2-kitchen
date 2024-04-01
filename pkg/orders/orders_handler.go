abe73158962542ceef4150d7340e4e607cb1049f
Dmitrii B <dmitriibb616@gmail.com>
2024-04-01 09:39:10 +0300

    validate grpc input data

--- pkg/orders/orders_handler.go
+++ pkg/orders/orders_handler.go
@@ -2,6 +2,8 @@ package orders
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"github.com/dmitriibb/go-common/logging"
 )
 
@@ -19,6 +21,12 @@ var OrdersHandler = &ordersHandler{NewOrders: make(chan *PutNewOrderRequest, 100
 
 func (ko *ordersHandler) PutNewOrder(ctx context.Context, in *PutNewOrderRequest) (*PutNewOrderResponse, error) {
 	loggerService.Debug("Received new order %s", in)
+
+	if len(in.Items) == 0 {
+		errMsg := fmt.Sprintf("Received order without any items: %+v", in)
+		loggerService.Error(errMsg)
+		return &PutNewOrderResponse{Status: "error"}, errors.New(errMsg)
+	}
 
 	if in.Items[0].Comment == "kitchen error" {
 		loggerService.Warn("Fake error on receiving new order. Cancel ctx")
